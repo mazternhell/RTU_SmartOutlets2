565899aa943186c6d48909df5582a036adf0561e
mazternhell <nel_dolot@yahoo.com>
2017-09-25 19:51:16 +0800

    V1.2 -app updates from backend includes 3 outlets

--- app/src/main/java/smartoutlets/elecdesignworks/ph/smartoutlets/MainActivity.java
+++ app/src/main/java/smartoutlets/elecdesignworks/ph/smartoutlets/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -18,18 +19,25 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import java.util.concurrent.CountDownLatch;
 
-public class MainActivity extends AppCompatActivity{
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private final String TAG = MainActivity.class.getSimpleName();
-    private Switch togglebtn_1;
-    private Switch togglebtn_2;
-    private Switch togglebtn_3;
+    private Button  btn_1;
+    private Button  btn_2;
+    private Button  btn_3;
+    private Button  btn_4;
+    private Button  btn_5;
+    private Button  btn_6;
     private TextView push_status;
     private TextView out1_status;
     private TextView out2_status;
     private TextView out3_status;
     private TextView detection_status;
+
+    final CountDownLatch countDownLatch = new CountDownLatch(1);
 
     String push_success;
     String field_json;
@@ -45,148 +53,210 @@ public class MainActivity extends AppCompatActivity{
 
         mainApplication = MainApplication.getMainApplication();
 
-        togglebtn_1 = (Switch) findViewById(R.id.togglebtn_1);
-        togglebtn_2 = (Switch) findViewById(R.id.togglebtn_2);
-        togglebtn_3 = (Switch) findViewById(R.id.togglebtn_3);
+        btn_1 = (Button) findViewById(R.id.btn_1);
+        btn_2 = (Button) findViewById(R.id.btn_2);
+        btn_3 = (Button) findViewById(R.id.btn_3);
+        btn_4 = (Button) findViewById(R.id.btn_4);
+        btn_5 = (Button) findViewById(R.id.btn_5);
+        btn_6 = (Button) findViewById(R.id.btn_6);
+
         push_status = (TextView) findViewById(R.id.push_status);
         out1_status = (TextView) findViewById(R.id.out1_status);
         out2_status = (TextView) findViewById(R.id.out2_status);
         out3_status = (TextView) findViewById(R.id.out3_status);
         detection_status = (TextView) findViewById(R.id.detection_status);
 
- //       push_status.setVisibility(View.INVISIBLE);
+        btn_1.setOnClickListener(this);
+        btn_2.setOnClickListener(this);
+        btn_3.setOnClickListener(this);
+        btn_4.setOnClickListener(this);
+        btn_5.setOnClickListener(this);
+        btn_6.setOnClickListener(this);
 
         final android.os.Handler handler = new android.os.Handler();
         Runnable run = new Runnable() {
+
             @Override
             public void run() {
 
                 //    pullData(final String results,final String field,final String read_api_key, String channel, String url)
 
-                if (count==1){
-
-                    pullData("1", "KDJJ75KTBLQZKDKM","336368","https://api.thingspeak.com/channels/"); //LOAD 1 sensor
-
-//                    push_status.setText("Movement " + field_json);
-
-                    if (result==0) {
+                if (count == 1) {
+                    pullData("1", "KDJJ75KTBLQZKDKM", "336368", "https://api.thingspeak.com/channels/"); //LOAD 1 sensor
+                    if (result == 0) {
                         detection_status.setText("NO MOTION DETECTED");
                         detection_status.setTextColor(Color.RED);
                     }
-                    if (result==1) {
+                    if (result == 1) {
                         detection_status.setText("MOTION DETECTED");
                         detection_status.setTextColor(Color.GREEN);
-
                     }
-
                 }
 
-                if (count==2) {
+                if (count == 2) {
                     pullData("1", "4LZPK4K4DNTMROS1", " 336369", "https://api.thingspeak.com/channels/"); //LOAD 2 Sensor
-
-//                    push_status.setText("Load 1 " + field_json);
-
-                    if (result==0) {
+                    if (result == 0) {
                         out1_status.setText("Device Unplugged");
                         out1_status.setTextColor(Color.RED);
                     }
-                    if (result==1) {
+                    if (result == 1) {
                         out1_status.setText("Device plugged in");
                         out1_status.setTextColor(Color.GREEN);
 
                     }
                 }
-                if (count==3){
-                    pullData("1", "S2CEY98ZBPJZC5MN","336370","https://api.thingspeak.com/channels/"); //LOAD 3 sensor
-
-  //                  push_status.setText("Load 2 " + field_json);
-
-                    if (result==0) {
+                if (count == 3) {
+                    pullData("1", "S2CEY98ZBPJZC5MN", "336370", "https://api.thingspeak.com/channels/"); //LOAD 3 sensor
+                    if (result == 0) {
                         out2_status.setText("Device Unplugged");
                         out2_status.setTextColor(Color.RED);
                     }
-                    if (result==1) {
+                    if (result == 1) {
                         out2_status.setText("Device plugged in");
                         out2_status.setTextColor(Color.GREEN);
-
                     }
 
                 }
-                if (count>3){
-                    pullData("1", "4JZQUJ74J32CG0B2","336371","https://api.thingspeak.com/channels/"); //Motion sensor
 
-//                    push_status.setText("Load 3 " + field_json);
-
-                    if (result==0) {
+                if (count == 4) {
+                    if (result == 0) {
+                        pullData("1", "WPC1Y6PMG5Y5XQNC", "336357", "https://api.thingspeak.com/channels/"); //Motion sensor
                         out3_status.setText("Device Unplugged");
                         out3_status.setTextColor(Color.RED);
                     }
-                    if (result==1) {
+                    if (result == 1) {
                         out3_status.setText("Device plugged in");
                         out3_status.setTextColor(Color.GREEN);
 
                     }
-                    count=0;
+                    pullData("1", "WPC1Y6PMG5Y5XQNC", "336357", "https://api.thingspeak.com/channels/"); //Motion sensor
+                }
+
+                if (count == 5) {
+
+                    pullData("1", "Q1NNITEM9903EUSQ", "336363", "https://api.thingspeak.com/channels/"); //Motion sensor
+
+                    if(result==0){
+                        btn_1.setBackgroundColor(Color.GRAY);
+                        btn_2.setBackgroundColor(Color.RED);
+                    }
+                    else if(result>0){
+                        btn_1.setBackgroundColor(Color.GREEN);
+                        btn_2.setBackgroundColor(Color.GRAY);
+                    }
+
+                }
+                if (count == 6) {
+                    pullData("1", "1TS5ALVNZXAJOA33", "336364", "https://api.thingspeak.com/channels/"); //Motion sensor
+
+                    if(result==0){
+                        btn_3.setBackgroundColor(Color.GRAY);
+                        btn_4.setBackgroundColor(Color.RED);
+                    }
+                    else if(result>0){
+                        btn_3.setBackgroundColor(Color.GREEN);
+                        btn_4.setBackgroundColor(Color.GRAY);
+                    }
+
+                }
+                if (count == 7) {
+                    pullData("1", "4JZQUJ74J32CG0B2", "336371", "https://api.thingspeak.com/channels/"); //Motion sensor
+                    if(result==0){
+                        btn_5.setBackgroundColor(Color.GRAY);
+                        btn_6.setBackgroundColor(Color.RED);
+                    }
+                    else if(result>0){
+                        btn_5.setBackgroundColor(Color.GREEN);
+                        btn_6.setBackgroundColor(Color.GRAY);
+                    }
+                    count = 0;
                 }
 
                 count++;
-                handler.postDelayed(this,2000);
+                handler.postDelayed(this, 2000);
             }
         };
 
         handler.post(run);
+    }
 
         // FUNCTIONS FOR PUSHING DATA to THINGSPEAK FOR OUTLET CONTROL
 
-        togglebtn_1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
-                if (isChecked){
-                    push_status.setText("OUTLET 1 ON");
-                    push_status.setVisibility(View.VISIBLE);
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_1:
                     sendData("field1", "1", "L3UOJ3ZX7UIIFS0H", "https://api.thingspeak.com/update");
-                }
-                else if(!isChecked){
-                    push_status.setText("OUTLET 1 OFF");
-                    push_status.setVisibility(View.VISIBLE);
+
+                    if(result==0){
+                        btn_1.setBackgroundColor(Color.GRAY);
+                        btn_2.setBackgroundColor(Color.RED);
+                    }
+                        else if(result>0){
+                        btn_1.setBackgroundColor(Color.GREEN);
+                        btn_2.setBackgroundColor(Color.GRAY);
+                    }
+                    break;
+
+                case R.id.btn_2:
                     sendData("field1", "0", "L3UOJ3ZX7UIIFS0H", "https://api.thingspeak.com/update");
-                }
-            }
-        });
+                    if (result==0){
+                        btn_1.setBackgroundColor(Color.GREEN);
+                        btn_2.setBackgroundColor(Color.GRAY);
+                    }
+                        else if (result>0){
+                        btn_1.setBackgroundColor(Color.GRAY);
+                        btn_2.setBackgroundColor(Color.RED);
+                    }
+                    break;
 
-        togglebtn_2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked){
-                    push_status.setText("OUTLET 2 ON");
-                    push_status.setVisibility(View.VISIBLE);
+                case R.id.btn_3:
                     sendData("field1", "1", "8JMJL8B7C24WQTC7", "https://api.thingspeak.com/update");
-                }
-                else if(!isChecked){
-                    push_status.setText("OUTLET 2 OFF");
-                    push_status.setVisibility(View.VISIBLE);
+                    if(result==0){
+                        btn_3.setBackgroundColor(Color.GRAY);
+                        btn_4.setBackgroundColor(Color.RED);
+                    }
+                    else if(result>0){
+                        btn_3.setBackgroundColor(Color.GREEN);
+                        btn_4.setBackgroundColor(Color.GRAY);
+                    }
+                    break;
+                case R.id.btn_4:
                     sendData("field1", "0", "8JMJL8B7C24WQTC7", "https://api.thingspeak.com/update");
-                }
-            }
-        });
-
-        togglebtn_3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked){
-                    push_status.setText("OUTLET 3 ON");
-                    push_status.setVisibility(View.VISIBLE);
+                    if (result==0){
+                        btn_3.setBackgroundColor(Color.GREEN);
+                        btn_4.setBackgroundColor(Color.GRAY);
+                    }
+                    else if (result>0){
+                        btn_3.setBackgroundColor(Color.GRAY);
+                        btn_4.setBackgroundColor(Color.RED);
+                    }
+                    break;
+                case R.id.btn_5:
                     sendData("field1", "1", "BCHMAJCGEDVWNQNE", "https://api.thingspeak.com/update");
-                }
-                else if(!isChecked){
-                    push_status.setText("OUTLET 3 OFF");
-                    push_status.setVisibility(View.VISIBLE);
+                    if(result==0){
+                        btn_5.setBackgroundColor(Color.GRAY);
+                        btn_6.setBackgroundColor(Color.RED);
+                    }
+                    else if(result>0){
+                        btn_5.setBackgroundColor(Color.GREEN);
+                        btn_6.setBackgroundColor(Color.GRAY);
+                    }
+                    break;
+                case R.id.btn_6:
                     sendData("field1", "0", "BCHMAJCGEDVWNQNE", "https://api.thingspeak.com/update");
-                }
+                    if (result==0){
+                        btn_5.setBackgroundColor(Color.GREEN);
+                        btn_6.setBackgroundColor(Color.GRAY);
+                    }
+                    else if (result>0){
+                        btn_5.setBackgroundColor(Color.GRAY);
+                        btn_6.setBackgroundColor(Color.RED);
+                    }
+                    break;
             }
-        });
-    }
+
+        }
 
     private void sendData(final String valueid,final String value,final String write_api_key, String url) {
         String tag_json_obj = "json_obj_req";
@@ -198,12 +268,14 @@ public class MainActivity extends AppCompatActivity{
                     @Override
                     public void onResponse(String response) {
                         push_success=response;
+                        result = Integer.parseInt(push_success);
                         push_status.setText(push_success);
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 push_success=error.toString();
+                result = Integer.parseInt(push_success);
                 push_status.setText(push_success);
             }
         }){
@@ -231,12 +303,17 @@ public class MainActivity extends AppCompatActivity{
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
+
+                        countDownLatch.countDown();
+
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 push_success=error.toString();
                 push_status.setText(push_success);
+                countDownLatch.countDown();
+
             }
         }){
         };
